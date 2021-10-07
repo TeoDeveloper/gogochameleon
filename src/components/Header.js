188672e2ee36d6bd5b2d56a5429e79002140e2fc
Matteo Marino <mat.marino90@gmail.com>
2021-10-07 13:46:56 +0200

    set header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,6 @@ import logoGGC from '../images/gogochameleon-logo.svg';
 import logoEye from '../images/logoEye.svg';
 import pupil from '../images/pupil.svg';
 
-const isMobile = navigator.userAgent.match(/Mobile/i);
 class Header extends Component {
     render() {
         const { description = '' } = this.props;
@@ -11,14 +10,13 @@ class Header extends Component {
             <>
                 <div id="header" className="header-section">
                     <div className="container header-container d-flex justify-content-center align-items-center flex-column">
-                        {isMobile ? <img className="static-logo mx-auto" src={logoGGC} alt={'logo'}/> :
-                            <div className="logo-container">
-                                <img className="logo" src={logoEye} width="200" alt={'logo_eye'}/>
-                                <div id="eye">
-                                    <img src={pupil} width="30" alt={'logo_pupil'}/>
-                                </div>
+                        <img className="static-logo mx-auto d-block d-lg-none" src={logoGGC} alt={'logo'}/>
+                        <div className="logo-container d-none d-lg-block">
+                            <img className="logo" src={logoEye} width="200" alt={'logo_eye'}/>
+                            <div id="eye">
+                                <img src={pupil} width="30" alt={'logo_pupil'}/>
                             </div>
-                        }
+                        </div>
                         <div className="header-title text-center">
                             <h2 className="montserrat fw900 color-bgprimary"
                                 dangerouslySetInnerHTML={{__html: 'GoGoChameleon'}}/>
@@ -31,67 +29,65 @@ class Header extends Component {
         )
     }
     componentDidMount() {
-        if(!isMobile) {
-            const EYES = {};
-            let mouseY, elY, obj;
-            EYES.stack = [];
-            EYES.follow = function(eye) {
-                let el1 = document.getElementById(eye);
-                let el2 = el1.getElementsByTagName("IMG");
-                el2 = el2[0];
-                el2.centre = (el1.offsetWidth - el2.offsetWidth) / 2;
-                el2.style.margin = (el2.centre - 10) + "px 0 0 " + (el2.centre + 8) + "px";
-                el1.maxDist = 6 * el1.offsetWidth;
-                EYES.stack.push([el1, el2]);
-            };
+        const EYES = {};
+        let mouseY, elY, obj;
+        EYES.stack = [];
+        EYES.follow = function(eye) {
+            let el1 = document.getElementById(eye);
+            let el2 = el1.getElementsByTagName("IMG");
+            el2 = el2[0];
+            el2.centre = (el1.offsetWidth - el2.offsetWidth) / 2;
+            el2.style.margin = (el2.centre - 10) + "px 0 0 " + (el2.centre + 8) + "px";
+            el1.maxDist = 6 * el1.offsetWidth;
+            EYES.stack.push([el1, el2]);
+        };
 
-            EYES.mousemove = function(e) {
-                if (!EYES.stack.length) {
-                    return;
+        EYES.mousemove = function(e) {
+            if (!EYES.stack.length) {
+                return;
+            }
+
+            let mouseX = mouseY = 0;
+            if (!e) e = window.event;
+            if (e.pageX || e.pageY) {
+                mouseX = e.pageX;
+                mouseY = e.pageY;
+            } else if (e.clientX || e.clientY) {
+                mouseX = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+                mouseY = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+            }
+
+            for (let e = 0, l = EYES.stack.length; e < l; e++) {
+                let el = EYES.stack[e];
+                let elX = elY = 0;
+                if (el[0].offsetParent) {
+                    obj = el[0];
+                    do {
+                        elX += obj.offsetLeft;
+
+                        elY += obj.offsetTop;
+                    } while (!!(obj = obj.offsetParent));
                 }
 
-                let mouseX = mouseY = 0;
-                if (!e) e = window.event;
-                if (e.pageX || e.pageY) {
-                    mouseX = e.pageX;
-                    mouseY = e.pageY;
-                } else if (e.clientX || e.clientY) {
-                    mouseX = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-                    mouseY = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-                }
+                el[0].centreX = elX + Math.round(el[0].offsetWidth / 2);
+                el[0].centreY = elY + Math.round(el[0].offsetHeight / 2);
+                let difX = mouseX - el[0].centreX;
+                let difY = mouseY - el[0].centreY;
+                let dTan = Math.atan2(difX, difY);
+                let dist = Math.sqrt((difX * difX) + (difY * difY));
+                dist = Math.sin((dist > el[0].maxDist ? 1 : dist / el[0].maxDist));
+                let newX = el[1].centre + Math.round(Math.sin(dTan) * el[1].centre * dist);
+                let newY = el[1].centre + Math.round(Math.cos(dTan) * el[1].centre * dist);
+                el[1].style.margin = newY + "px 0 0 " + newX + "px";
+            }
+        };
 
-                for (let e = 0, l = EYES.stack.length; e < l; e++) {
-                    let el = EYES.stack[e];
-                    let elX = elY = 0;
-                    if (el[0].offsetParent) {
-                        obj = el[0];
-                        do {
-                            elX += obj.offsetLeft;
+        if (window.attachEvent) document.attachEvent("onmousemove", EYES.mousemove);
+        else document.addEventListener("mousemove", EYES.mousemove, false);
 
-                            elY += obj.offsetTop;
-                        } while (!!(obj = obj.offsetParent));
-                    }
-
-                    el[0].centreX = elX + Math.round(el[0].offsetWidth / 2);
-                    el[0].centreY = elY + Math.round(el[0].offsetHeight / 2);
-                    let difX = mouseX - el[0].centreX;
-                    let difY = mouseY - el[0].centreY;
-                    let dTan = Math.atan2(difX, difY);
-                    let dist = Math.sqrt((difX * difX) + (difY * difY));
-                    dist = Math.sin((dist > el[0].maxDist ? 1 : dist / el[0].maxDist));
-                    let newX = el[1].centre + Math.round(Math.sin(dTan) * el[1].centre * dist);
-                    let newY = el[1].centre + Math.round(Math.cos(dTan) * el[1].centre * dist);
-                    el[1].style.margin = newY + "px 0 0 " + newX + "px";
-                }
-            };
-
-            if (window.attachEvent) document.attachEvent("onmousemove", EYES.mousemove);
-            else document.addEventListener("mousemove", EYES.mousemove, false);
-
-            (function() {
-                EYES.follow ('eye');
-            })();
-        }
+        (function() {
+            EYES.follow ('eye');
+        })();
     }
 }
 
